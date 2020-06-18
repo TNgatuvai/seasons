c61b9d8aa9d58f641363365e1cb15d3e538f8a81
Francesca Ngatuvai <Chekkabekka@Talis-MacBook-Pro.local>
2020-06-18 01:50:11 -0600

    replaced const w/ class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    // call to get user's location 
+
+class App extends React.Component {
+    render() {
+        // call to get user's location 
     window.navigator.geolocation.getCurrentPosition( 
         (position) => console.log(position),
         (err) => console.log(err)
     );
 
-    return <div>Latitude:</div>
-};
+        return <div>Latitude:</div>;
+    }
+}
 
-ReactDOM.render (
-    <App />,
-    document.querySelector("#root")
-);
+
+ReactDOM.render (<App />, document.querySelector("#root"));
